833f363431a94537f1680abcf6245edd6e16007a
naveennallamsetti <137390505+naveennallamsetti@users.noreply.github.com>
2026-03-04 13:29:59 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,5 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven Project!");
+         System.out.println("Hello, Maven naveen!");
     }
 }
+
